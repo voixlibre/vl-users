e6b393c67a95dfdf5d79dac1290a0774b12ed1fd
julien <juliencauwet@yahoo.fr>
2019-04-15 10:24:50 +0200

    changed methods path and added method to save user

--- src/main/java/org/greenwin/VLUsers/controllers/UserController.java
+++ src/main/java/org/greenwin/VLUsers/controllers/UserController.java
@@ -1,7 +1,5 @@
 package org.greenwin.VLUsers.controllers;
 
-
-
 import org.greenwin.VLUsers.configuration.ApplicationPropertiesConfiguration;
 import org.greenwin.VLUsers.entities.AppUser;
 import org.greenwin.VLUsers.repositories.AppRoleRepository;
@@ -9,14 +7,14 @@ import org.greenwin.VLUsers.repositories.AppUserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -30,20 +28,25 @@ public class UserController {
     @Autowired
     ApplicationPropertiesConfiguration configuration;
 
-    @GetMapping("/users")
+    @GetMapping("/")
     public List<AppUser> getAllUsers(){
         logger.info("entering getAllUsers()");
         logger.info("configuration test: " + configuration.getTest());
         return appUserRepository.findAll();
-
     }
 
-    @GetMapping("/users/{id}")
+    @GetMapping("/id/{id}")
     public AppUser getUserById(@PathVariable("id") int id){
         AppUser user = appUserRepository.getAppUserById(id);
         List<AppUser> users = new ArrayList<>();
         users.add(user);
-
         return user;
     }
+
+    @PostMapping("/")
+    public AppUser saveUser(@RequestBody AppUser appUser){
+        return appUserRepository.save(appUser);
+    }
+
+
 }
---
+++ src/main/java/org/greenwin/VLUsers/utils/Paths.java
@@ -0,0 +1,5 @@
+package org.greenwin.VLUsers.utils;
+
+public class Paths {
+    private final static String USER_DEFAULT= "/users";
+}
